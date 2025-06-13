f1c98ddf3797919def3a0d87f4b56228b25f796e
Juan Diego <jdiego.jauregui.0115@gmail.com>
2025-06-12 19:55:40 -0600

    Hola segundo commit

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -25,5 +26,25 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        val btnGoMenu = findViewById<Button>(R.id.btn_go_to_menu)
+        btnGoMenu.setOnClickListener {
+            goToMenu()
+        }
+
+        val tvGoRecover = findViewById<TextView>(R.id.tv_go_to_recover)
+        tvGoRecover.setOnClickListener {
+            goToRecover()
+        }
+    }
+
+    private fun goToMenu() {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToRecover() {
+        val intent = Intent(this, RecoverActivity::class.java)
+        startActivity(intent)
     }
 }
